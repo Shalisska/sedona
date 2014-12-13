93bf0419f78b890ddf8ce96cfb16bd01d28b3a15
Shalisska <alena.simonova83@gmail.com>
2014-12-13 19:29:42 +0300

    счетчик

--- sedona_js/js/script.js
+++ sedona_js/js/script.js
@@ -1,69 +1,32 @@
-(function(document) {
-    
+(function (document) {
+	
 var counters = document.querySelectorAll('.counter');
-
+    
 for (var i = 0; i < counters.length; i++) {
-    var counter = counters[i];
-    
-    var minus = counter.querySelector('.minus');
-    var plus = counter.querySelector('.plus');
-    var result = counter.querySelector('.counter-input');
-    
-    
+    	
+	var count = function (j) {
+		var counter = counters[j];
+		
+		var minus = counter.querySelector('.minus');
+		var plus = counter.querySelector('.plus');
+		var result = counter.querySelector('.counter-input');
+		var val = parseInt(result.value, 10);
+		var res = val;
+		
+		plus.addEventListener('click', function (event) {
+			event.preventDefault ();
+			res = ++val;
+			result.value = res;
+		});
 
-    plus.addEventListener('click', function (event) {
-        event.preventDefault ();
-        var val = parseInt(result.value, 10);
-        var res = val;
-        res = ++val;
-        result.value = res;
-        console.log(res);
-    });
-
-    minus.addEventListener('click', function (event) {
-        event.preventDefault ();
-        var val = parseInt(result.value, 10);
-        var res = val;
-        res = --val;
-        result.value = res;
-        console.log(res);
-    });
-    
-    
-    console.log(result);
+		minus.addEventListener('click', function (event) {
+			event.preventDefault ();
+			res = --val;
+			result.value = res;
+		});
+	} (i);
 };
 
-
-    
-
-    
-
-    
-    
-    
-    
-    
-
-//var minus = document.querySelector('.test .minus');
-//var plus = document.querySelector('.test .plus');
-//var result = document.querySelector('.test .counter-input');
-//var val = parseInt(result.value, 10);
-//var res = val;
-//    
-//plus.addEventListener('click', function (event) {
-//    event.preventDefault ();
-//    res = ++val;
-//    result.value = res;
-//    console.log(res);
-//});
-//    
-//minus.addEventListener('click', function (event) {
-//    event.preventDefault ();
-//    res = --val;
-//    result.value = res;
-//    console.log(res);
-//});
-    
 
 })(document);
 
