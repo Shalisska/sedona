64c24536c66db08f8d4099a5f1fa1b555f44c110
Shalisska <alena.simonova83@gmail.com>
2014-12-23 09:47:16 +0300

    sedona_js

--- sedona_js/js/script.js
+++ sedona_js/js/script.js
@@ -1,150 +1,149 @@
-(function (document) {
-
-	
-	
-var counters = document.querySelectorAll('.counter');
-    
-for (var i = 0; i < counters.length; i++) {
-    	
-	var count = function (j) {
-		var counter = counters[j];
-		
-		var minus = counter.querySelector('.minus');
-		var plus = counter.querySelector('.plus');
-		var result = counter.querySelector('.counter-input');
-		
-		var val = parseInt(result.value, 10);
-		var res = val;
-		
-		plus.addEventListener('click', function (event) {
-			event.preventDefault ();
-			res = ++val;
-			result.value = res;
-		});
-
-		minus.addEventListener('click', function (event) {
-			event.preventDefault ();
-			
-			if (result.classList.contains('children-count') & val > 0) {
-				res = --val;
-				result.value = res;
-			} else if (val > 1) {
-				res = --val;
-				result.value = res;
-			};
-		});
-		
-		var getChar = function (event) {
-			if (event.which == null) {
-				if (event.keyCode < 32) return null;
-				return String.fromCharCode(event.keyCode) // IE
-			}
-
-			if (event.which!=0 && event.charCode!=0) {
-				if (event.which < 32) return null;
-				return String.fromCharCode(event.which)   // остальные
-			}
-
-		return null; // специальная клавиша
-		}
-
-		result.onkeypress = function(e) {
-			e = e || event;
-			var chr  = getChar(e);
-
-			if (e.ctrlKey || e.altKey || chr == null) return; // специальная клавиша
-			if (chr < '0' || chr > '9') return false;
-		}
-	} (i);
-};
-	
-
-})(document);
-//
-////<form>
-////<input type="button" value="- 1" onClick="javascript:this.form.numeric.value--;">
-////<input type="text" name="numeric" size="4" value="555">
-////<input type="button" value="+ 1" onClick="javascript:this.form.numeric.value++;">
-////</form>
-//
 //(function (document) {
 //
-//
-//var form = document.querySelector('.response');
-//
-//var data = new FormData(form);	
-//var xhr = new XMLHttpRequest ();
 //	
-////xhr.open('post', '/echo?' + (new Date()).getTime());
-////	
-////xhr.onreadystatechange = function () {
-////	if (xhr.readyState == 4) {
-////		console.log(xhr.responseText);
-////	}
-////};
-////	
-////xhr.send(data);
-//
-//
-//var area = document.querySelector('.photo-area');
-////var queue = [];
 //	
-//form.querySelector('.pictures').addEventListener('change', function () {
-//	var files = this.files;
+//var counters = document.querySelectorAll('.counter');
+//    
+//for (var i = 0; i < counters.length; i++) {
+//    	
+//	var count = function (j) {
+//		var counter = counters[j];
 //		
-//	previewImages (files);		
-//});
-//						  
-//function previewImages (files) {
-//	for (var i = 0; i < files.length; i++) {
-//		var file = files[i];
+//		var minus = counter.querySelector('.minus');
+//		var plus = counter.querySelector('.plus');
+//		var result = counter.querySelector('.counter-input');
 //		
-//		if (file.type.match(/image.*/)) {
-//			var reader = new FileReader();
-//				
-//			reader.onload = function (event) {
-//				var container = document.createElement('div');
-//				container.classList.add('photo');
-//				area.appendChild(container);
-//				
-//				var del = document.createElement('a');
-//				del.classList.add('delete-photo');
-//				container.appendChild(del);
-//				
-//				var img = document.createElement('img');
-//				img.src = event.target.result;
-//				img.alt = '';
-//				container.appendChild(img);
+//		var val = parseInt(result.value, 10);
+//		var res = val;
+//		
+//		plus.addEventListener('click', function (event) {
+//			event.preventDefault ();
+//			res = ++val;
+//			result.value = res;
+//		});
+//
+//		minus.addEventListener('click', function (event) {
+//			event.preventDefault ();
+//			
+//			if (result.classList.contains('children-count') & val > 0) {
+//				res = --val;
+//				result.value = res;
+//			} else if (val > 1) {
+//				res = --val;
+//				result.value = res;
 //			};
-//				
-//			reader.readAsDataURL(file);
+//		});
+//		
+//		var getChar = function (event) {
+//			if (event.which == null) {
+//				if (event.keyCode < 32) return null;
+//				return String.fromCharCode(event.keyCode) // IE
+//			}
+//
+//			if (event.which!=0 && event.charCode!=0) {
+//				if (event.which < 32) return null;
+//				return String.fromCharCode(event.which)   // остальные
+//			}
+//
+//		return null; // специальная клавиша
 //		}
-//	}	
-//}	
+//
+//		result.onkeypress = function(e) {
+//			e = e || event;
+//			var chr  = getChar(e);
+//
+//			if (e.ctrlKey || e.altKey || chr == null) return; // специальная клавиша
+//			if (chr < '0' || chr > '9') return false;
+//		}
+//	} (i);
+//};
 //	
-//	
-//	
-//	
-//	
-//	
-//	
+//
 //})(document);
 
-//(function(document) {
-//    for (var item of document.querySelectorAll('.counter .counter-action')) {
-//        item.addEventListener('click', function (event) {
-//            event.preventDefault();
-//            var inp = this.parentNode.querySelector('.counter-input');
-//            var val = parseInt(inp.value, 10);
-//            if (this.classList.contains('plus')) {
-//                inp.value = ++val;
-//            } else if (this.classList.contains('minus')){
-//                inp.value = --val;
-//            }
-//        });
-//    }
-//})(document);
+(function (document) {
 
+
+	var form = document.querySelector('.response');
+	var area = document.querySelector('.photo-area');
+
+	var queue = [];
+
+	var data = new FormData(form);
+//	queue.forEach(function (element) {
+//		data.append('pictures', element.file);
+//	});
+
+	var xhr = new XMLHttpRequest();
+	
+//xhr.open('post', '/echo?' + (new Date()).getTime());
+//	
+//xhr.onreadystatechange = function () {
+//	if (xhr.readyState == 4) {
+//		console.log(xhr.responseText);
+//	}
+//};
+//	
+//xhr.send(data);
+
+
+	form.querySelector('.pictures').addEventListener('change', function () {
+		var files = this.files;
+		previewImages(files);
+		
+		this.value = '';
+	});
+
+	function previewImages(files) {
+		for (var i = 0; i < files.length; i++) {
+			var file = files[i];
+
+			if (file.type.match(/image.*/)) {
+				var reader = new FileReader();
+
+				reader.onload = function (event) {
+//					var container = document.createElement('div');
+//					container.classList.add('photo');
+//					area.appendChild(container);
+//
+//					var del = document.createElement('a');
+//					del.classList.add('delete-photo');
+//					del.href = '#';
+//					del.textContent = 'Удалить';
+//					container.appendChild(del);
+//
+//					var img = document.createElement('img');
+//					img.src = event.target.result;
+//					img.alt = '';
+//					container.appendChild(img);
+				
+				
+				
+				
+					var template = document.querySelector('#photo-template').innerHTML;
+				
+					var html = Mustache.render(template, {
+						photo: event.target.result,
+						name: file.name
+					});
+
+				area.innerHTML = area.innerHTML + html;	
+				
+//					queue.push ({file: file, img: img});
+				};
+
+				reader.readAsDataURL(file);
+			}
+		}	
+	}	
+	
+	
+	
+	
+	
+	
+	
+})(document);
 
 
 
